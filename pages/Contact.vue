fb299b468a712219ba3726fc4176e3e19b58603f
hughvanosdall <hughvanosdall@gmail.com>
2024-01-02 00:01:02 -0800

    contact page early.

---
+++ pages/Contact.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-sheet>
+    <v-form @submit.prevent>
+      <v-text-field
+        v-model="Name"
+        :rules="rules"
+        label="Name"
+      ></v-text-field>
+
+      <v-text-field 
+      v-model="Name"
+      :rules="rules"
+      label="Email"
+      placeholder="example@domain.com"
+      ></v-text-field>
+
+      <v-text-field 
+      label="Tell us what you need"
+      ></v-text-field>
+
+      <v-btn
+      type="submit" 
+      block class="mt-2"
+      >Submit</v-btn>
+    </v-form>
+  </v-sheet>
+</template>
+  
+<script setup>
+    import { ref } from "vue";
+    
+    const rules = ref(
+        [
+            value => {
+              if (value) return true    
+              return 'You must enter a name.'
+            },
+        ]
+    );
+</script>
+
+<style>
+    v-sheet{
+        padding: 100rem;
+    }
+    p {
+        color: black;
+    };
+</style>
+  
