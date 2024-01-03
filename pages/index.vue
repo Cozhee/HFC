6ff744e0503f7d61ab87e4dbe1f41d8a18fc1305
John Cripe <johntc121@gmail.com>
2024-01-03 01:09:42 -0800

    spacing fix

--- pages/index.vue
+++ pages/index.vue
@@ -29,6 +29,7 @@
         </v-col>
       </v-row>
     </v-container>
+    <HomePageAboutUs />
   </div>
 </template>
 
@@ -36,7 +37,7 @@
 
 <style scoped>
 .home-page {
-  height: 70vh;
+  min-height: 70vh;
 }
 
 .home-page-banner {
