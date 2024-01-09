6fddb8087362fcb8bfb9f00b114b5743db03f923
hughvanosdall <hughvanosdall@gmail.com>
2024-01-09 15:26:09 -0800

    Added pricing page, adjusted nuxt links on About page

--- components/HomePage/AboutUs.vue
+++ components/HomePage/AboutUs.vue
@@ -90,22 +90,23 @@
                 </v-col>
               </v-row>
             </v-container>
-            <v-btn rounded="xl" class="about-btn">
-              <NuxtLink class="link-btn" to="/about"> ABOUT US </NuxtLink>
-              &nbsp;
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="19"
-                height="17"
-                viewBox="0 0 19 17"
-                fill="none"
-              >
-                <path
-                  d="M9.0835 0.303605C9.31963 0.101224 9.54265 0.101224 9.75255 0.303605L17.9779 8.27234C18.2141 8.47472 18.2141 8.6771 17.9779 8.87948L9.75255 16.8482C9.54265 17.0506 9.31963 17.0506 9.0835 16.8482L8.29638 16.0893C8.21767 16.0134 8.17831 15.9122 8.17831 15.7857C8.17831 15.6592 8.21767 15.5454 8.29638 15.4442L14.3965 9.56251H0.976177C0.66133 9.56251 0.503906 9.41073 0.503906 9.10716V8.04466C0.503906 7.74109 0.66133 7.5893 0.976177 7.5893H14.3965L8.29638 1.70762C8.08648 1.47994 8.08648 1.26491 8.29638 1.06253L9.0835 0.303605Z"
-                  fill="white"
-                />
-              </svg>
-            </v-btn>
+            <NuxtLink class="link-btn" to="/about">
+              <v-btn rounded="xl" class="about-btn">
+                ABOUT US&nbsp;
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="19"
+                  height="17"
+                  viewBox="0 0 19 17"
+                  fill="none"
+                >
+                  <path
+                    d="M9.0835 0.303605C9.31963 0.101224 9.54265 0.101224 9.75255 0.303605L17.9779 8.27234C18.2141 8.47472 18.2141 8.6771 17.9779 8.87948L9.75255 16.8482C9.54265 17.0506 9.31963 17.0506 9.0835 16.8482L8.29638 16.0893C8.21767 16.0134 8.17831 15.9122 8.17831 15.7857C8.17831 15.6592 8.21767 15.5454 8.29638 15.4442L14.3965 9.56251H0.976177C0.66133 9.56251 0.503906 9.41073 0.503906 9.10716V8.04466C0.503906 7.74109 0.66133 7.5893 0.976177 7.5893H14.3965L8.29638 1.70762C8.08648 1.47994 8.08648 1.26491 8.29638 1.06253L9.0835 0.303605Z"
+                    fill="white"
+                  />
+                </svg>
+              </v-btn>
+            </NuxtLink>
           </div>
         </v-col>
       </v-row>
@@ -160,10 +161,8 @@ const getImageUrl = (path) => {
   flex-shrink: 0;
   border-radius: 300px 300px 0px 0px;
   object-fit: none;
-  background:
-    url(../../assets/images/About/aboutus_people.png),
-    lightgray 50% / cover no-repeat,
-    #d9d9d9;
+  background: url(../../assets/images/About/aboutus_people.png),
+    lightgray 50% / cover no-repeat, #d9d9d9;
   z-index: 1;
 }
 
@@ -206,6 +205,7 @@ const getImageUrl = (path) => {
 .about-btn {
   margin-top: 35px;
   background: linear-gradient(90deg, #5956f0 0%, #ff50b8 100%);
+  color: white;
 }
 
 .experience-box {
--- pages/Pricing.vue
+++ pages/Pricing.vue
@@ -1,4 +1,155 @@
-<template>Pricing</template>
-<script></script>
-<style scope></style>
-    
+<template>
+  <v-container fluid class="mt-6 v-container-wrapper">
+    <v-container>
+      <v-row class="mb-16">
+        <v-col>
+          <div class="d-flex flex-column">
+            <div class="d-flex flex-column">
+              <div class="text-center">
+                <v-icon
+                  icon="mdi-network-pos purple-pink-gradient text-center"
+                ></v-icon>
+                <h4 class="d-inline purple-pink-gradient ms-2">
+                  Affordable Pricing For All
+                </h4>
+              </div>
+              <div class="global-header text-center">Chose Your Plan</div>
+            </div>
+            <div class="w-100">
+              <p class="mission-text">
+                Contact HJC for a free quote with no credit card required.<br />
+                You can upgrade anytime and whatever progress we made<br />(including
+                any Edits we made for your site) will still be there!
+              </p>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+      <v-row
+        class="d-flex flex-rows align-center justify-center mb-16 pricing-card-container"
+      >
+        <v-col cols="auto">
+          <v-card
+            class="text-center pricing-cards"
+            width="300"
+            varient="elevated tonal"
+            title="Fixed"
+          >
+            <v-card-text>
+              Pick from a variety of designs all for one up front cost.
+            </v-card-text>
+            <h2>Starting at $1000</h2>
+            <p>Whats Included?</p>
+            <p class="">&#x2713; Tailored exactly to your needs</p>
+            <p class="">&#x2713; Tailored exactly to your needs</p>
+            <p class="pb-3">&#x2713; Tailored exactly to your needs</p>
+          </v-card>
+        </v-col>
+        <v-col cols="auto" class="pricing-big-boi">
+          <v-card
+            class="text-center pricing-cards pricing-middle-card"
+            width="300"
+            varient="elevated"
+            title="Monthly"
+            ><v-card-text>
+              Affordable monthly pricing with added benefits.
+            </v-card-text>
+            <h2>$150/month</h2>
+            <p>Whats Included?</p>
+            <p class="">&#x2713; Tailored exactly to your needs</p>
+            <p class="">&#x2713; Tailored exactly to your needs</p>
+            <p class="pb-3">&#x2713; Tailored exactly to your needs</p>
+          </v-card>
+        </v-col>
+        <v-col cols="auto">
+          <v-card
+            class="text-center pricing-cards"
+            width="300"
+            varient="elevated"
+            title="Custom"
+          >
+            <v-card-text>
+              Custom built from the ground up for all your business needs.
+            </v-card-text>
+            <h2>Flexible Pricing</h2>
+            <p>Whats Included?</p>
+            <p class="">&#x2713; Tailored exactly to your needs</p>
+            <p class="">&#x2713; Tailored exactly to your needs</p>
+            <p class="pb-3">&#x2713; Tailored exactly to your needs</p>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row class="d-flex flex-column pricing-ready-container">
+        <v-col cols="auto">
+          <div class="d-flex flex-column">
+            <p class="global-header text-center">Ready to get started?</p>
+          </div>
+          <div
+            class="d-flex flex-column justify-center align-center pricing-contact-us-container"
+          >
+            <p class="mission-text text-center">Get your free quote now!</p>
+            <NuxtLink to="/contact">
+              <v-btn class="mt-2 pricing-button" @click="">Contact Us</v-btn>
+            </NuxtLink>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-container>
+</template>
+<script setup></script>
+<style scoped>
+.pricing-card-container .v-col {
+  display: flex;
+}
+.focus-text {
+  color: #fff;
+  font-size: 60px;
+  font-weight: 700;
+  text-align: center;
+  line-height: 82.3px;
+}
+
+.mission-text {
+  color: #e1e1e1;
+  font-family: Space Grotesk;
+  font-size: 19px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 30px;
+  margin-top: 35px;
+  text-align: center;
+}
+
+.v-card {
+  background: gray;
+  color: white;
+}
+
+.pricing-cards {
+  background-color: rgb(52, 52, 52);
+  padding: 1.15rem;
+}
+
+.pricing-button {
+  background: linear-gradient(180deg, #7255e7 0%, #e050c2 100%);
+  color: white;
+  text-transform: up;
+}
+.pricing-middle-card {
+  scale: 1.1;
+}
+
+@media screen and (width < 1280px) {
+  .pricing-middle-card {
+    scale: 1;
+  }
+  .pricing-contact-us-container {
+    position: relative;
+    top: -30px;
+  }
+  .v-container-wrapper {
+    padding-bottom: 0px;
+  }
+}
+</style>
